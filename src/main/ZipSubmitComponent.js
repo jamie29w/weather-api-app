b3690a2eea8759f8b9c533642a94f30c7aba6315
Jamie Woodmancy <jamiew@Jamies-MacBook-Pro.local>
2018-05-27 14:04:20 -0700

    building out zip input component

---
+++ src/main/ZipSubmitComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ZipSubmitComponent() {
+    return (
+        <div>
+            <input id='zip' placeholder='zip'/>
+            <button id='zipSubmit'>Check Local Weather</button>
+        </div>
+    )
+}
+
+export default ZipSubmitComponent;
