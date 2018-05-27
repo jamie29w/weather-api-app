afd5588f3731a97471eed1531a5b06dd6a3730ea
Jamie Woodmancy <jamie29w@gmail.com>
2018-05-26 22:17:43 -0700

    working on getting user geolocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-    return <div>hi from App.js</div>
+class App extends Component {
+
+    componentWillMount() {
+        let location = navigator.geolocation.getCurrentPosition(showPosition);
+        function showPosition (position) {
+            var userPosition = {};
+            userPosition.lat = position.latitude;
+            userPosition.long = position.longitude;
+            return userPosition
+        }
+        console.log(location);
+    }
+
+    
+    
+
+    render(props) {
+        
+        return <div></div>
+    }
 }
 
 export default App
