fad816bb248bddb026c7f49516f783eadbaac5c0
Jamie Woodmancy <jamie29w@gmail.com>
2018-05-27 14:04:20 -0700

    building out zip input component

---
+++ src/main/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ZipSubmitComponent from './ZipSubmitComponent'
+
+function App () {   
+    return (
+            <ZipSubmitComponent/>
+            // <div>hi</div>
+    )
+    
+}
+
+export default App
