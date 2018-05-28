b5e4777b3f5d4c72eed48b12fd91e11396d9b154
Jamie Woodmancy <jamie29w@gmail.com>
2018-05-28 10:28:50 -0700

    wired up request for lat & lng through google api

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ZipSubmitComponent from './ZipSubmitComponent'
+import axios from 'axios';
 
-function App () {   
-    return (
-            <ZipSubmitComponent/>
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            lat: null,
+            lng: null,
+        }
+        this.zipSubmit = this.zipSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+
+    // componentDidMount() {
+    //     axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCZNng4wnr3pcGCRRotJxDGMDVxYLqh9I8`).then(res => {
+    //         console.log(res)
+    //     })
+    // }
+
+    handleChange(lat,lng) {
+        this.setState(prevState => {
+            if (true) {
+                return {
+                    lat: lat,
+                    lng: lng
+                }
+            } else {
+                return prevState;
+            }
+        })
+        console.log(this.state)
+        console.log(typeof this.state.lat)
+    }
+
+    zipSubmit(zip) {
+        axios.post(`https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=AIzaSyCZNng4wnr3pcGCRRotJxDGMDVxYLqh9I8
+        `).then(res => {
+            const coords = res.data.results[0].geometry.location
+            // console.log(res.data.results[0].geometry.location.lat)
+            // console.log(res.data.results[0].geometry.location.lng)
+            this.handleChange(coords.lat, coords.lng)
+        })
+    }
+
+
+    render() {
+        return (
+            <ZipSubmitComponent
+                zipSubmit = {this.zipSubmit}
+            />
             // <div>hi</div>
     )
+    }
     
 }
 
